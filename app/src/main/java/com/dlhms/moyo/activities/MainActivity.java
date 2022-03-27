7fd8c63e98528f268e5eafaab5cd88fd8c27f004
Ikay <84293264+Ikay-real@users.noreply.github.com>
2022-03-27 11:47:32 -0700

    Initial commit

---
+++ app/src/main/java/com/dlhms/moyo/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package com.dlhms.moyo.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.dlhms.moyo.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
